d0e038c27104961f9166a4876f02567e8b0ffe14
Near_River <798563420@qq.com>
2016-05-07 14:15:01 +0800

    fundamental

--- fundamental/py_01.py
+++ fundamental/py_01.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 # input     output
 print('Hello World!')
 print('1+2= ', 1+2)
--- fundamental/py_03.py
+++ fundamental/py_03.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 # if - elif - else
 age = int(input('Input your age: '))
 if age >= 18:
