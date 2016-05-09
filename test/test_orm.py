5ec39a75fa47abbb0bc2c5b0572006699fea562e
Near_River <798563420@qq.com>
2016-05-09 23:07:59 +0800

    test_orm

--- test/test_orm.py
+++ test/test_orm.py
@@ -1,14 +1,91 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-    create a simple orm frame:
-"""
+""" create a simple orm frame: """
 
 __author__ = 'Nate River'
 
 
+class Field(object):
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name):
+        super(StringField, self).__init__(name, 'varchar(64)')
+
+
+class IntegerField(Field):
+    def __init__(self, name):
+        super(IntegerField, self).__init__(name, 'int')
+
+
+class ModelMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        print('Found model: %s' % name)
+        mappings = dict()
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                print('Found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+        for k in mappings.keys():
+            attrs.pop(k)
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = name  # 假设表名和类名一致
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaClass):
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def save(self):
+        fields = []
+        params = []
+        args = []
+        for k, v in self.__mappings__.items():
+            fields.append(v.name)
+            params.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
+
+class User(Model):
+    id = IntegerField('id')
+    name = StringField('username')
+    email = StringField('email')
+    password = StringField('password')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return 'Id: %s Name: %s Email: %s Password: %s' % (self.id, self.name, self.email, self.password)
 
 
 if __name__ == '__main__':
-    pass
+    u = User(id=101, name='Admin', email='admin@server.com', password='admin')
+    u.save()
+    # print('model mappings: ', u.__mappings__)
+    # print('table name: ', u.__table__)
+    # print(u)
