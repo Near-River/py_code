27449e5b05fcb797331f3fafc8ef1d06fcd64bbd
Near_River <798563420@qq.com>
2016-05-13 21:00:59 +0800

    test

---
+++ test/test_sax.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nate River'
+
+from xml.parsers.expat import ParserCreate
+from collections import OrderedDict
+
+
+class WeatherSaxHandler(object):
+
+    def __init__(self):
+        self.weather = OrderedDict()
+        self.weather['today'] = dict()
+        self.weather['tomorrow'] = dict()
+
+    def start_element(self, name, attrs):
+        if 'day' in attrs.keys() and 'Wed' == attrs['day']:
+            for k, v in attrs.items():
+                try:
+                    self.weather['today'][k] = int(v)
+                except ValueError:
+                    self.weather['today'][k] = v
+        elif 'day' in attrs.keys() and 'Thu' == attrs['day']:
+            for k, v in attrs.items():
+                try:
+                    self.weather['tomorrow'][k] = int(v)
+                except ValueError:
+                    self.weather['tomorrow'][k] = v
+        else:
+            for k, v in attrs.items():
+                self.weather[k] = v
+
+    def end_element(self, name):
+        pass
+
+    def character_data(self, text):
+        pass
+
+
+def parse_weather(xml):
+    sax_handler = WeatherSaxHandler()
+    parser = ParserCreate()
+    parser.StartElementHandler = sax_handler.start_element
+    parser.EndElementHandler = sax_handler.end_element
+    parser.CharacterDataHandler = sax_handler.character_data
+    parser.Parse(xml)
+    return sax_handler.weather
+
+
+if __name__ == '__main__':
+    data = r'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+    <rss version="2.0" xmlns:yweather="http://xml.weather.yahoo.com/ns/rss/1.0" xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#">
+        <channel>
+            <title>Yahoo! Weather - Beijing, CN</title>
+            <lastBuildDate>Wed, 27 May 2015 11:00 am CST</lastBuildDate>
+            <yweather:location city="Beijing" region="" country="China"/>
+            <yweather:units temperature="C" distance="km" pressure="mb" speed="km/h"/>
+            <yweather:wind chill="28" direction="180" speed="14.48" />
+            <yweather:atmosphere humidity="53" visibility="2.61" pressure="1006.1" rising="0" />
+            <yweather:astronomy sunrise="4:51 am" sunset="7:32 pm"/>
+            <item>
+                <geo:lat>39.91</geo:lat>
+                <geo:long>116.39</geo:long>
+                <pubDate>Wed, 27 May 2015 11:00 am CST</pubDate>
+                <yweather:condition text="Haze" code="21" temp="28" date="Wed, 27 May 2015 11:00 am CST" />
+                <yweather:forecast day="Wed" date="27 May 2015" low="20" high="33" text="Partly Cloudy" code="30" />
+                <yweather:forecast day="Thu" date="28 May 2015" low="21" high="34" text="Sunny" code="32" />
+                <yweather:forecast day="Fri" date="29 May 2015" low="18" high="25" text="AM Showers" code="39" />
+                <yweather:forecast day="Sat" date="30 May 2015" low="18" high="32" text="Sunny" code="32" />
+                <yweather:forecast day="Sun" date="31 May 2015" low="20" high="37" text="Sunny" code="32" />
+            </item>
+        </channel>
+    </rss>
+    '''
+    weather = parse_weather(data)
+    assert weather['city'] == 'Beijing', weather['city']
+    assert weather['country'] == 'China', weather['country']
+    assert weather['today']['text'] == 'Partly Cloudy', weather['today']['text']
+    assert weather['today']['low'] == 20, weather['today']['low']
+    assert weather['today']['high'] == 33, weather['today']['high']
+    assert weather['tomorrow']['text'] == 'Sunny', weather['tomorrow']['text']
+    assert weather['tomorrow']['low'] == 21, weather['tomorrow']['low']
+    assert weather['tomorrow']['high'] == 34, weather['tomorrow']['high']
+    for k, v in weather.items():
+        # print(k, ':', v)
+        pass
