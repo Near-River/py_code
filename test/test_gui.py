cdf83ccd9575a31b558ad34bc40df1a542f99435
Near_River <798563420@qq.com>
2016-05-14 12:55:28 +0800

    gui

---
+++ test/test_gui.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nate River'
+
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        # pack(): 把Widget加入到父容器中
+        self.pack()
+        self.nameEntry = Entry(self)
+        # self.exitButton = Button(self, text='Exit', command=self.quit)
+        self.triggerButton = Button(self, text='Click', command=self.sayHello)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameEntry.pack()
+        self.triggerButton.pack()
+
+    def sayHello(self):
+        name = self.nameEntry.get() or 'anonymous'
+        messagebox.showinfo(title='Message', message='hello, %s' % name)
+
+
+if __name__ == '__main__':
+    app = Application()
+    # 设置窗口标题
+    app.master.title = 'App'
+    # 主消息循环
+    app.mainloop()
