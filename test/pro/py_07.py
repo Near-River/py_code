c1b90499e526bda71b7f753ceb4f3cea641cf1bf
Near_River <798563420@qq.com>
2016-05-14 10:26:02 +0800

    test

--- test/pro/py_07.py
+++ test/pro/py_07.py
@@ -2,22 +2,9 @@
 from lib2to3.pgen2 import parse
 from urllib import request
 import http.cookiejar
-from threading import Thread
-from queue import Queue
-from time import sleep
 from urllib.parse import urlencode
 
 # 网络爬虫
-# =========================================================================
-
-resp = request.urlopen('http://www.baidu.com')
-# print(resp.getcode())  # 获取请求状态码
-# print(resp.read().decode('utf-8'))  # 获取网页内容
-
-req = request.Request('http://www.baidu.com')
-# data：向服务器提交需要用户输入的数据  header：向服务器提交一个http的头信息
-req.add_header('user-agent', 'Mozilla/5.0')
-
 # =========================================================================
 # 添加特殊情景的处理器：
 #   1.需要用户登录：HTTPCookieProcessor
@@ -64,51 +51,9 @@ for cookie in cj:
 # req = request.Request(full_url)
 
 
-# 伪装成浏览器访问
-# post_data = urlencode({})
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
-# }
-# req = request.Request(
-#     url='http://secure.verycd.com/signin/*/http://www.verycd.com/',
-#     data=post_data,
-#     headers=headers
-# )
-
 # =========================================================================
 # 反”反盗链”：把headers的referer改成该网站即可，
 # headers = {
 #     'Referer': 'http://www.cnbeta.com/articles'
 # }
 # 用selenium直接控制浏览器来进行站点访问，类似的还有pamie，watir，...
-
-
-# 模拟多线程并发抓取
-# q是任务队列    NUM是并发线程总数  JOBS是有多少任务
-q, NUM, JOBS = Queue(), 3, 10
-
-
-# 具体处理函数，负责处理单个任务
-def do_somthing(arguments):
-    print(arguments)
-
-
-# 工作进程，负责不断从队列中取数据并处理
-def working():
-    while True:
-        arguments = q.get()
-        do_somthing(arguments)
-        sleep(1)
-        q.task_done()
-
-
-# fork NUM个线程等待队列
-for i in range(NUM):
-    t = Thread(target=working)
-    t.setDaemon(True)  # 设置为守护线程
-    t.start()
-# 把JOBS排入队列
-for i in range(JOBS):
-    q.put('Job：%d' % (i + 1))
-# 等待所有JOBS完成
-q.join()  # join()：Blocks until all items in the Queue have been gotten and processed.
---
+++ test/test_10.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import threading, time
+from queue import Queue
+from threading import Thread
+
+
+# 模拟多线程并发抓取
+# q是任务队列    NUM是并发线程总数  JOBS是有多少任务
+q, NUM, JOBS = Queue(), 3, 10
+
+
+# 具体处理函数，负责处理单个任务
+def do_somthing(arguments):
+    print('current thread %s: ' % threading.current_thread().name, arguments)
+
+
+# 工作进程，负责不断从队列中取数据并处理
+def working():
+    while True:
+        arguments = q.get()
+        do_somthing(arguments)
+        time.sleep(1)
+        q.task_done()
+
+# fork NUM个线程等待队列
+for i in range(NUM):
+    t = Thread(target=working)
+    t.setDaemon(True)  # 设置为守护线程
+    t.start()
+
+# 把JOBS排入队列
+for i in range(JOBS):
+    q.put('Job：%d' % (i + 1))
+
+# 等待所有JOBS完成
+q.join()  # join()：Blocks until all items in the Queue have been gotten and processed.
